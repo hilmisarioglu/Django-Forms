87b645f5499e1727616839db025fda2463d01ea8
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-12-11 19:56:47 +0100

    v2

--- student/models.py
+++ student/models.py
@@ -4,6 +4,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     number = models.IntegerField(blank=True, null=True)
+    number2 = models.IntegerField(blank=True, null=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
